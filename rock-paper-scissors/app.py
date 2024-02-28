f5ebc58f7c15216c85784961d6286bf904583079
LeserraJ <leserrabusiness@gmail.com>
2024-02-28 21:47:19 +0000

    added rock paper scissors

---
+++ rock-paper-scissors/app.py
@@ -0,0 +1,45 @@
+import random
+
+#array of choices
+throws = ["rock","paper","scissor"]
+
+#computer choice
+comp_throw = random.choice(throws)
+
+#user choice
+user_throw = (input("What do you want to throw?\n Rock, Paper, or Scissor?\n"))
+
+
+#Rock functionality
+def rock_function():
+    if user_throw == throws[0] and comp_throw == throws[0]:
+        print("tie")
+    elif comp_throw == throws[1]:
+        print("lose")
+    else:
+        print("win")
+
+
+#Paper functionality
+def paper_function():
+    if user_throw == throws[1] and comp_throw == throws[1]:
+        print("tie")
+    elif comp_throw == throws[2]:
+        print("lose")
+    else:
+        print("win")
+
+
+#Scissor functionality
+def scissor_function():
+    if user_throw == throws[2] and comp_throw == throws[2]:
+        print("tie")
+    elif comp_throw == throws[0]:
+        print("lose")
+    else:
+        print("win")
+
+
+
+
+print(comp_throw)
