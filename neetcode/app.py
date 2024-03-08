3b222de38cf6eef27a66da8ab41aa7356c85ab03
LeserraJ <leserrabusiness@gmail.com>
2024-03-08 19:03:21 +0000

    function works

--- neetcode/app.py
+++ neetcode/app.py
@@ -5,10 +5,24 @@ tasks = input("Enter Item Here. \n")
 todoList = [tasks]
 
 
+user_change = input("Would you like to make a change? \n")
+
+
 def remove_function(): 
-    input("Would you like to make a change? \n")
+    print("Here are the Items:\n", tasks)
     removeTask = todoList.remove(input("Which Item to Remove? \n"))
+   
+
+if user_change.upper() == "YES":
+    remove_function()
+    print(todoList)
+else:
+    print(todoList)
 
 
 
-print(todoList)
+
+
+
+
+
