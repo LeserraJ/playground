139c1d3f48ff53d601ec822423081ee60357e2da
LeserraJ <leserrabusiness@gmail.com>
2024-03-20 20:11:51 +0000

    added C3

--- number-guesser/app.py
+++ number-guesser/app.py
@@ -1,13 +1,42 @@
 import random
-
-
-start = int(input("Please enter the min number in the range.\n " ))
-stop = int(input("Please enter the max number in the range.\n " ))
-
-
-
-#random.randint(start, stop)
-computer_number = random.randint(start,stop)
-
-
-print(computer_number)
+import math
+# Taking Inputs
+lower = int(input("Enter Lower bound:- "))
+ 
+# Taking Inputs
+upper = int(input("Enter Upper bound:- "))
+ 
+# generating random number between
+# the lower and upper
+x = random.randint(lower, upper)
+print("\n\tYou've only ", 
+       round(math.log(upper - lower + 1, 2)),
+      " chances to guess the integer!\n")
+ 
+# Initializing the number of guesses.
+count = 0
+ 
+# for calculation of minimum number of
+# guesses depends upon range
+while count < math.log(upper - lower + 1, 2):
+    count += 1
+ 
+    # taking guessing number as input
+    guess = int(input("Guess a number:- "))
+ 
+    # Condition testing
+    if x == guess:
+        print("Congratulations you did it in ",
+              count, " try")
+        # Once guessed, loop will break
+        break
+    elif x > guess:
+        print("You guessed too small!")
+    elif x < guess:
+        print("You Guessed too high!")
+ 
+# If Guessing is more than required guesses,
+# shows this output.
+if count >= math.log(upper - lower + 1, 2):
+    print("\nThe number is %d" % x)
+    print("\tBetter Luck Next time!")
